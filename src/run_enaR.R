79e8815c37c9ad9c8203cf893e7c7f6ed86fac7f
mkl <mkl48@nau.edu>
2014-06-02 13:34:27 -0400

    reduced to model loading and put in windows path

--- src/run_enaR.R
+++ src/run_enaR.R
@@ -14,26 +14,33 @@ rm(list=ls())
 library(enaR)
 
 # file.name is the name of plain text SCOR formatted ENA model, to be provided by the calling tool
-file.name="%SCORFILE%"
-                                        #file.name='../debugging/data/Namibia output for Stuart.dat'
+                                        #file.name="%SCORFILE%"
+                                        #Unix/Linux
+                                        #file.name <- '../data/Deepfish output for Stuart.dat'
+                                        #windows
+file.name <- '..\data\Deepfish output for Stuart.dat'
 
 # load model
 m <- read.scor(file.name)          # read in model
-m <- balance(m)                    # balances model if needed
+print(m)
+print('Done reading model.')
 
-# perform analyses
-All <- list()
-All$A <- enaStructure(m)           # structure analysis
-All$F <- enaFlow(m)                # flow analysis
-All$S <- enaStorage(m)
-All$U <- enaUtility(m,eigen.check=FALSE)
-All$C <- enaControl(m)
-All$mti <- enaMTI(m,eigen.check=FALSE)
-All$E <- enaEnviron(m)
+## ### balancing
+## m <- balance(m)                    # balances model if needed
 
-# writes output to a txt file.  
-zz <- file("tmp.txt",open="wt")
-sink(zz)
-show(All)
-sink()
-unlink(zz)
+## # perform analyses
+## All <- list()
+## All$A <- enaStructure(m)           # structure analysis
+## All$F <- enaFlow(m)                # flow analysis
+## All$S <- enaStorage(m)
+## All$U <- enaUtility(m,eigen.check=FALSE)
+## All$C <- enaControl(m)
+## All$mti <- enaMTI(m,eigen.check=FALSE)
+## All$E <- enaEnviron(m)
+
+## # writes output to a txt file.  
+## zz <- file("tmp.txt",open="wt")
+## sink(zz)
+## show(All)
+## sink()
+## unlink(zz)
